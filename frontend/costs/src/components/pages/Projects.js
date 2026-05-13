8907f79dcdc4404dc8db96852f1b2c36aa979feb
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-05-13 16:48:36 -0300

    update

--- frontend/costs/src/components/pages/Projects.js
+++ frontend/costs/src/components/pages/Projects.js
@@ -1,4 +1,7 @@
+import styles from './Projects.module.css'
 import Mensagem from "./layout/Mensagem"
+import Container from './layout/Container'
+import Linkbottun from './layout/Linkbottun'
 import { useLocation } from "react-router-dom"
 function Projects(){
 
@@ -9,11 +12,17 @@ function Projects(){
     }
 return (
 
-    <div>
-        <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+       <div className={ styles.title_container}>
+         <h1>Meus Projetos</h1>
+      <Linkbottun to='/newproject' text="Criar Projeto">Novo projeto</Linkbottun>
+        </div>
 
     {message && <Mensagem type='sucess' mgs={message} />}
     
+    <Container customClass="start">
+        <p>Projetos</p>
+    </Container>
     </div>
 
 )
